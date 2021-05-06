306801c83ccc470aa11cbc54624c134298a3bc28
Heber Sandoval <he3ers@gmail.com>
2021-05-06 10:55:03 -0400

    Created a simple web server to serve some text to the browser via the home() handler function.

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+// home is a handler function which writes a byte slice as the response body.
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+func main() {
+	// Initialize a new servemux, then register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// Start a new web server and pass the TCP network address to listen on and the servemux just created.
+	// If http.ListenAndServe() returns an error, an error will be thrown.
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
 }
