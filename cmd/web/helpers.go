ee9acc558c1017fb5031d00aa94ed6a1fd587794
Heber Sandoval <he3ers@gmail.com>
2021-06-23 14:04:55 -0400

    Move comments to helpers.go and clean up code in handlers.go.

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -10,12 +10,13 @@ import (
 // generic 500 Internal Server Error response to the user.
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	app.errorLog.Println(trace)
+	app.errorLog.Output(2, trace) // Report one step back in the stack trace.
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
 // clientError helper sends a specific status code and corresponding description to the user.
 func (app *application) clientError(w http.ResponseWriter, status int) {
+	// Use the http.Error() function that uses w.WriteHeader() and w.Write() under the hood to send a string as the response body.
 	http.Error(w, http.StatusText(status), status)
 }
 
