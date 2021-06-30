25b1b4d0d07975759e444d84762ded1701306112
Heber Sandoval <he3ers@gmail.com>
2021-06-30 14:24:57 -0400

    Insert() method implementation to insert values to db.

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -12,7 +12,21 @@ type SnippetModel struct {
 
 // Insert inserts new snippet into db.
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+		VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+	// Use the Exec() method on the embedded connection pool to execute the statement.
+	// This method returns a sql.Result object.
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+	// Use the LastInsert() method on the result object to get the ID of our newly inserted record in the snippet table.
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	// The ID returned has the type int64, so we convert it to an int type before returning.
+	return int(id), nil
 }
 
 // Get return a specific snippet based on its id.
