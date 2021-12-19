3a9b62d790743db7b58a747076f3dc48e28f030f
Heber Sandoval <he3ers@gmail.com>
2021-12-19 08:11:32 -0500

    Added a field for holding a slice of snippets.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -5,5 +5,6 @@ import "github.com/hebersandoval/snippetbox/pkg/models"
 // Define a templateData type to act as the holding structure for any dynamic data that
 // we want to pass to our HTML templates.
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
